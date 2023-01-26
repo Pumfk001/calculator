2c71eb900b630b02e6e758744623c8b1ca4b119d
Pumfk001 <110676191+Pumfk001@users.noreply.github.com>
2023-01-26 19:51:38 +0000

    Add ability to calculate first round and display

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
 
+
+const numberInput = document.querySelectorAll('.numberInput');
+const operatorInput = document.querySelectorAll('.operatorInput');
+const displayVal = document.querySelector('.display');
+const finalAns = document.querySelector('.equals');
+
+let firstNum = '';
+let secondNum = '';
+let tempNum = '';
+let operatorType = '';
+
+displayVal.textContent = '0';
+
+//use inputs from button clicks
+numberInput.forEach(item => {
+    item.addEventListener('click', event => {
+        let buttonVal = item.textContent;
+        if(displayVal.textContent==='÷' || displayVal.textContent==='x' || displayVal.textContent==='-' || displayVal.textContent==='+'){
+            firstNum = tempNum;
+            tempNum = '';
+        };
+        tempNum += buttonVal;
+        changeDisplay(tempNum);
+    })
+});
+
+operatorInput.forEach(item => {
+    item.addEventListener('click', event => {
+        if(operatorType!=='') calcAns(); 
+        console.log(tempNum);
+        operatorType = item.textContent;
+        changeDisplay(operatorType);
+    })
+});  
+
+function clearAll(){
+    answer = 'undefined'
+    firstNum = '';
+    secondNum = '';
+    tempNum = '';
+    operatorType = '';
+    changeDisplay(0);
+}
+
+function calcAns(){
+    tempNum = operate(operatorType, firstNum, tempNum);
+    changeDisplay(tempNum);
+}
+
+function changeDisplay(input){
+    displayVal.textContent = input;
+};
+
+
 //create basic functions to call
 
 function add (a, b){
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 };
 
 function subtract (a, b){
@@ -32,38 +86,4 @@ function operate (x, a, b){
 
 };
 
-const numberInput = document.querySelectorAll('.numberInput');
-const operatorInput = document.querySelectorAll('.operatorInput');
-const displayValue = document.querySelector('.display');
-let firstNum = ''
-let secondNum = ''
-
-//use inputs from button clicks
-numberInput.forEach(item => {
-    item.addEventListener('click', event => {
-
-    // console.log(displayValue.textContent);
-    let buttonVal = item.textContent;
-
-    
-    // if(displayValue.textContent==='x'){
-    //     secondNum += buttonVal;
-    //     changeDisplay(secondNum);
-    // };
-        firstNum += buttonVal;
-        changeDisplay(firstNum);
-    })
-});
-
-operatorInput.forEach(item => {
-    item.addEventListener('click', event => {
-        let operatorType = item.textContent;
-        changeDisplay(operatorType);
-    })
-});  
-
-
-
-function changeDisplay(input){
-    displayValue.textContent = input;
-};
+// ERROR AFTER CALCULATION AFTER EQUALS NOT WORKING CORRECTLY
